149d21b00067b467fce72c487dd08598eb006968
cimyjh@gmail.com <developer123@yjh>
2020-07-26 18:47:04 +0900

    init

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://cimyjh:<developer@mongocim>@dmc-init.5g37d.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}
+    ).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
